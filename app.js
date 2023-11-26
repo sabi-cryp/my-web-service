e1da5ed3b52aecd5e311377004b3fcd882721c78
sabi-cryp <sbrinekerchaoui3@gmail.com>
2023-11-27 00:08:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// Import necessary modules
+const express = require('express');
+const mysql = require('mysql2');
+
+// Create an Express application
+const app = express();
+const port = 30001;
+
+// Set up MySQL connection
+const connection = mysql.createConnection({
+  host: '172.17.0.2', // Docker container name
+  user: 'dockeruser',
+  password: 'docker',
+  database: 'api',
+  port: 3306,
+});
+
+// Check if the connection to MySQL is successful
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    throw err;
+  }
+  console.log('Connected to MySQL database');
+});
+
+// Array of client data
+const clients = [
+  { id: 1, name: 'Sabrina', email: 'sabrina@example.com' },
+  { id: 2, name: 'Aymen', email: 'aymen@example.com' },
+  { id: 3, name: 'Mehdi', email: 'mehdi@example.com' },
+];
+
+// Define a route to get clients
+app.get('/get_clients', (req, res) => {
+  res.json(clients);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
